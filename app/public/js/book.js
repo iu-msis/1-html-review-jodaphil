6f7254eab45514dd06295dccb0b08df8ec19af04
jodaphil <jonathandphillips3@gmail.com>
2021-10-29 13:13:00 -0400

    Edit and delete books in table

--- app/public/js/book.js
+++ app/public/js/book.js
@@ -21,26 +21,78 @@ const bookTableApp = {
             })
         },
 
-        postNewBook(evt) {
-           
-            fetch('api/books/create.php', {
-                method:'POST',
-                body: JSON.stringify(this.bookForm),
-                headers: {
-                  "Content-Type": "application/json; charset=utf-8"
+        selectBook(o) {
+          this.selectedBook = o;
+          this.bookForm = Object.assign({}, this.selectedBook);
+        },
+
+      postNewBook(evt) {
+          //     this.offerForm.studentId = this.selectedStudent.id;
+
+              // console.log("Posting!", this.bookForm);
+              console.log("Posting!");
+
+              fetch('api/books/create.php', {
+                  method:'POST',
+                  body: JSON.stringify(this.bookForm),
+                  headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                  }
+                })
+                .then( response => response.json() )
+                .then( json => {
+                  console.log("Returned from post:", json);
+                  // TODO: test a result was returned!
+                  this.books = json;
+
+                  // reset the form
+                  this.bookForm = {};
+                });
+
+      },
+      postEditBook(evt) {
+                this.bookForm.id = this.selectedBook.id;
+
+
+                console.log("Updating!", this.bookForm);
+
+                fetch('api/books/update.php', {
+                    method:'POST',
+                    body: JSON.stringify(this.bookForm),
+                    headers: {
+                      "Content-Type": "application/json; charset=utf-8"
+                    }
+                  })
+                  .then( response => response.json() )
+                  .then( json => {
+                    console.log("Returned from post:", json);
+                    // TODO: test a result was returned!
+                    this.books = json;
+
+                    this.bookForm = {};
+                  });
+              },
+        postDeleteBook(book) {
+                if (!confirm("Are you sure you want to delete the record for "+book.title+"?")) {
+                      return;
                 }
-              })
-              .then( response => response.json() )
-              .then( json => {
-                
-                // TODO: test a result was returned!
-                this.books = json;
-                
-                // reset the form
-                this.bookForm = {};
-              });
+                console.log("Starting to Delete");
+                fetch('api/books/delete.php', {
+                method:'POST',
+                body: JSON.stringify(book),
+                headers: {
+                      "Content-Type": "application/json; charset=utf-8"
+                      }
+                  })
+                .then( response => response.json() )
+                .then( json => {
+                    console.log("Returned from post:", json);
+                    // TODO: test a result was returned!
+                    this.books = json;
+                    });
           }
     },
+       
     created() {
         this.fetchBooksData();
     }
